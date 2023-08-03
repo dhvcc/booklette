7db4980b8cbe4a2434f669df9a01998275176150
dhvcc <1337kwiz@gmail.com>
2023-08-03 20:52:25 +0300

    Update dialog, add signin and downloads buttons

--- lib/components/account_dialog.dart
+++ lib/components/account_dialog.dart
@@ -19,36 +19,59 @@ class AccountDialog extends StatelessWidget {
                   onPressed: () => Navigator.pop(context),
                   icon: const Icon(Icons.close),
                 ),
-                const Center(
-                  widthFactor: 3,
-                  child: Text("Booklette"),
-                ),
               ],
             ),
-            Center(
-              widthFactor: 3,
-              child: Column(
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: ListView(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Sign In"),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () {},
+                    child: const ListTile(
+                      leading: Icon(Icons.account_circle_outlined),
+                      title: Text("Sign In"),
                     ),
                   ),
-                  const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.close),
-                      Center(
-                        widthFactor: 3,
-                        child: Text("Settings"),
-                      ),
-                    ],
+                  InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () {},
+                    child: const ListTile(
+                      leading: Icon(Icons.download),
+                      title: Text("Downloads"),
+                    ),
                   ),
                 ],
               ),
-            ),
+            )
+            // Center(
+            //   widthFactor: 3,
+            //   child: Column(
+            //     children: [
+            //       const Row(
+            //         mainAxisSize: MainAxisSize.min,
+            //         children: [
+            //           Icon(Icons.account_circle_outlined),
+            //           Center(
+            //             widthFactor: 3,
+            //             child: Text("Sign in"),
+            //           ),
+            //         ],
+            //       ),
+            //       const Row(
+            //         mainAxisSize: MainAxisSize.min,
+            //         children: [
+            //           Icon(Icons.close),
+            //           Center(
+            //             widthFactor: 3,
+            //             child: Text("Settings"),
+            //           ),
+            //         ],
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
